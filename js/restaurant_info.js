78dc74a110a4bcc018fde2ec63e981883a9ba7da
Joseph Yalenkatian <josephyalenkatian@gmail.com>
2018-09-11 20:33:57 -0700

    chore: Add MapBox API Key & Add Viewport Tags

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoianlhbGVua2F0aWFuIiwiYSI6ImNqbHlrYXk1cjFrNWwzcGxpcDJxN2FpYW4ifQ.OEpXoOu4xiTDz_tPWbO6aQ',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
