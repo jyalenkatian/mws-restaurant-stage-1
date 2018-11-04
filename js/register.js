d25f969726511baaf2dacf8aeafa914ad1707b5a
Joseph Yalenkatian <josephyalenkatian@gmail.com>
2018-11-03 22:11:04 -0700

    Feat: Service Worker Files added for registering and using the service worker.

---
+++ js/register.js
@@ -0,0 +1,8 @@
+// File to set up the service worker
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/sw.js").then( reg => {
+        console.log("Serivce worker registration successful: " + reg.scope);
+    }).catch(error => {
+        console.log("Registration failed due to the following error: " + error);
+    })
+};
